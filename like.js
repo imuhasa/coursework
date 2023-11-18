7be8625708e47bb008fad0fe20367c45f7f01330
timur <Timur>
2023-11-18 23:23:36 +0300

    deleted useless lines of code

--- like.js
+++ like.js
@@ -1,14 +1,6 @@
 const likeButton = document.querySelectorAll('.main__card-like');
 console.log(likeButton);
 
-likeButton.onclick = function() {
-    for (let i = 0; i < likeButton.length; i++) {
-        likeButton.classList.toggle('main__card-like-active');
-        console.log(i);
-    }
-    
-}
-
 likeButton.forEach(e => e.addEventListener('click', () => {
     e.classList.toggle('main__card-like-active');
 }));
